7cbfdd007223578e4d38837a15c8fcee49fc277f
Jop <jopmolenaar@icloud.com>
2025-04-09 14:44:57 +0200

    feat(): check veiliginternetten fetch

--- background_scripts/background.js
+++ background_scripts/background.js
@@ -11,6 +11,41 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
             body: JSON.stringify(message.payload)
         });
     }
+
+    if (message.type === "checkVeiliginternetten") {
+        fetch(`https://check.veiliginternetten.nl/controleer/${message.url}`, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" }
+        })
+        .then(res => res.text())
+        .then(html => {
+            const isBetrouwbaar = html.includes("betrouwbaar");
+            const isOnbetrouwbaar = html.includes("niet veilig");
+            const jeMoetDrukken = html.includes("PLEASE PRESS THE BUTTON");
+            let result = "❓ Onbekend";
+
+            if (jeMoetDrukken) {
+                result = "❗️ Onbekend (downtime)";
+            } else if (isOnbetrouwbaar) {
+                result = "❌ Mogelijk onbetrouwbaar";
+            } else if (isBetrouwbaar) {
+                result = "✅ Betrouwbaar";
+            }
+            sendResponse({
+                result,
+                matched: isBetrouwbaar,
+                rawHtml: html // optioneel, alleen als je dat wil gebruiken
+            });
+        })
+        .catch(err => {
+            console.error("Fout bij ophalen:", err);
+            sendResponse({ error: err.toString() });
+        });
+
+        // Return true zorgt ervoor dat sendResponse async mag zijn
+        return true;
+    }
+
 });
 // chrome.action.onClicked.addListener((tab) => {
 //     const url = chrome.runtime.getURL("index.html"); // or whatever your internal page is
--- content_scripts/scan.js
+++ content_scripts/scan.js
@@ -1,28 +1,95 @@
+const domain = getRootDomain();
+
 async function injectUI() {
     document.body.style.border = "5px solid red"; // testing purposes
-
+    
+    // Popup HTML
     const html = await fetch(chrome.runtime.getURL("content_scripts/ui.html")).then(r => r.text());
-    const css = await fetch(chrome.runtime.getURL("content_scripts/style.css")).then(r => r.text());
-
-    const styleEl = document.createElement("style");
-    styleEl.textContent = css;
-    document.head.appendChild(styleEl);
-
     const wrapper = document.createElement("div");
     wrapper.innerHTML = html;
     document.body.appendChild(wrapper);
-
-
-    // Event listeners
-    const button = wrapper.querySelector("#openSettingsBtn");
+    wrapper.querySelector("#domainSpan").textContent = domain;
+    
+    // Popup CSS
+    const css = await fetch(chrome.runtime.getURL("content_scripts/style.css")).then(r => r.text());
+    const styleEl = document.createElement("style");
+    styleEl.textContent = css;
+    document.head.appendChild(styleEl);
+    
+    // Open settings page
     const homeButton = wrapper.querySelector("#openHomeBtn");
-    button.addEventListener("click", () => {
-        chrome.runtime.sendMessage({ action: "open_settings" });
-    });
     homeButton.addEventListener("click", () => {
         const url = chrome.runtime.getURL("index.html");
         window.open(url, "_blank");
     });
+
+    // Check safety of the domain
+    const checkButton = wrapper.querySelector("#check");
+    checkButton.addEventListener("click", checkSafetyDomain("checkVeiliginternetten", wrapper));
+
+
+    // Check all links, form actions and inputs of the webpage
+    const dangerousContent = checkWebContents();
+    if(dangerousContent){
+        console.log("Pas op! Er zijn verdachte elementen op deze pagina gevonden.");
+        // TODO markeer de elementen die verdacht zijn
+    }
 }
+
+function getRootDomain() {
+    const hostname = window.location.hostname;
+    const parts = hostname.split('.');
+
+    if (parts.length <= 2 || /^[0-9.]+$/.test(hostname) || hostname === 'localhost') {
+      return hostname;
+    }
+    
+    // Return the last two parts of the domain
+    return parts.slice(-2).join('.');
+}
+
+function checkSafetyDomain(source, wrapper) {
+    chrome.runtime.sendMessage({ type: source, url: domain }, (response) => {
+        wrapper.querySelector("#safety").textContent = domain + " is " + response.result;
+
+        const anchorSource = document.createElement("a");
+        anchorSource.href = "https://check.veiliginternetten.nl/controleer/" + domain;
+        anchorSource.target = "_blank";
+        anchorSource.textContent = "check.veiliginternetten.nl";
+        wrapper.querySelector("#source").textContent = "";
+        wrapper.querySelector("#source").appendChild(anchorSource);
+    });
+}
+
+function checkWebContents() {
+    const forms = document.body.querySelectorAll("form");
+    // console.log("Found forms:", forms);
+    forms.forEach((form) => {
+        const formData = new FormData(form);
+        const formObj = {};
+        formData.forEach((value, key) => {
+            formObj[key] = value;
+        });
+        // console.log("Form data:", formObj);
+
+        // Submits
+        const submitBtn = form.querySelector("button[type='submit']");
+        const submitInput = form.querySelector("input[type='submit']");
+        // console.log("submit:", submitBtn, submitInput);
+    });
+
+    const inputs = document.body.querySelectorAll("input");
+    inputs.forEach((input) => {
+        // console.log("Input name:", input.name);
+        // console.log("Input value:", input.value);
+    });
+
+    const anchors = document.body.querySelectorAll("a");
+    anchors.forEach((anchor) => {
+        // console.log("Anchor href:", anchor.href);
+        // console.log("Anchor text:", anchor.textContent);
+    });
+}
+
 
 window.onload = injectUI;
